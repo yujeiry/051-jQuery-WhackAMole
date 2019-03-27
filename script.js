7cf6d09b941beb884e4604a0ed3a45c9e9a9ecfe
yujeiry <43036901+yujeiry@users.noreply.github.com>
2019-03-27 08:59:24 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+$("#thirdMole").click(function(){
+    $("#thirdMole").hide();
+});
+    
+$("#show").click(function(){
+    $("#mole1").show();
+    $("moleTwo").show();
+    $("thirdMole").show();
+});
